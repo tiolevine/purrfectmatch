b216bb19592b77ae0aaba1d86ff29d61aae97358
Nathan T Levine <35020136+tiolevine@users.noreply.github.com>
2018-02-06 16:13:09 -0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// var lm = $(foo).dialog('open');
+// setTimeout(function() { lm.dialog('close'); }, 2000);
+
+$('#lm').click(function(){
+	// alert("MEOWWWWW");
+	setTimeout(function() 
+		{ alert("MEOWWWW"); }, 2000);
+});
+
+$("#mercury").css("color", "blue");
+
+$("#contact").click(function(){
+	alert("Human contact is made via the treat jar rattle!");
+});
+$("#about").click(function(){
+	alert("This is an age old question....");
+
+});
+$("#home").click(function(){
+	alert("Home is where the best treats are!");
+});
